353cb780eb5f6900c54ec2c1b642d1cbeaa146bd
John S Odom <john.s.odom@gmail.com>
2023-03-25 20:07:43 -0400

    Adding Time Quantity

---
+++ SI.Units.NET/Time.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SI.Units.NET
+{
+    /// <summary>
+    /// Represents Time Base Quantity type from Table 2. SI base units
+    /// </summary>
+    public readonly struct Time : IQuantity<Time>
+    {
+        /// <summary> Base unit of measure </summary>
+        public const Units BaseUnit = Units.Second;
+
+        /// <summary> Base symbol </summary>
+        public const string BaseSymbol = "m";
+
+        /// <summary> Supported units of measure for Time Quantity type </summary>
+        public enum Units
+        {
+            Second,
+            Decisecond,
+            Centisecond,
+            Millisecond,
+            Microsecond,
+            Nanosecond,
+            Picosecond,
+            Femtosecond,
+            Decasecond,
+            Hectosecond,
+            Kilosecond,
+            Megasecond,
+            Gigasecond,
+            Terasecond,
+            Petasecond,
+            Minute,
+            Hour,
+            Day
+        };
+
+        /// <summary>
+        /// Conversion factors from target unit to base unit.
+        /// </summary>
+        private static readonly double[] Factors =
+        {
+            1.0,
+            Prefixes.Deci.Factor,
+            Prefixes.Centi.Factor,
+            Prefixes.Milli.Factor,
+            Prefixes.Micro.Factor,
+            Prefixes.Nano.Factor,
+            Prefixes.Pico.Factor,
+            Prefixes.Femto.Factor,
+            Prefixes.Deca.Factor,
+            Prefixes.Hecto.Factor,
+            Prefixes.Kilo.Factor,
+            Prefixes.Mega.Factor,
+            Prefixes.Giga.Factor,
+            Prefixes.Tera.Factor,
+            Prefixes.Peta.Factor,
+            60.0,
+            3600.0,
+            86400.0
+        };
+
+        /// <summary>
+        /// Unit of measure symbols for each Unit. Order of
+        /// array must match Units order / int value
+        /// </summary>
+        private static readonly string[] Symbols =
+        {
+            BaseSymbol,
+            Prefixes.Deci.Symbol + BaseSymbol,
+            Prefixes.Centi.Symbol + BaseSymbol,
+            Prefixes.Milli.Symbol + BaseSymbol,
+            Prefixes.Micro.Symbol + BaseSymbol,
+            Prefixes.Nano.Symbol + BaseSymbol,
+            Prefixes.Pico.Symbol + BaseSymbol,
+            Prefixes.Femto.Symbol + BaseSymbol,
+            Prefixes.Deca.Symbol + BaseSymbol,
+            Prefixes.Hecto.Symbol + BaseSymbol,
+            Prefixes.Kilo.Symbol + BaseSymbol,
+            Prefixes.Mega.Symbol + BaseSymbol,
+            Prefixes.Giga.Symbol + BaseSymbol,
+            Prefixes.Tera.Symbol + BaseSymbol,
+            Prefixes.Peta.Symbol + BaseSymbol,
+            "min",
+            "hr",
+            "d"            
+        };
+
+        /// <summary>
+        /// Create new Time object
+        /// </summary>
+        /// <param name="value">Time value (amount)</param>
+        /// <param name="unit">Unit of measure value is in</param>
+        [JsonConstructor]
+        public Time(double value, Units unit)
+        {
+            Value   = value; 
+            Unit    = unit;
+        }
+
+        /// <inheritdoc/>
+        public double Value { get; init; }
+
+        /// <summary>
+        /// Unit of Measure value is in
+        /// </summary>
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Units Unit { get; init; }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return BaseValue().GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if ((obj is Time) == false) { return false; }
+            
+            return Equals((Time)obj);
+        }
+
+        /// <inheritdoc/>
+        public double BaseValue()
+        {
+            if (Unit == BaseUnit) return Value;
+
+            return Value * Factors[(int)Unit] / Factors[(int)BaseUnit];
+        }
+
+        /// <inheritdoc/>
+        public Time ToBase()
+        {
+            return As(BaseUnit);
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(Time other)
+        {
+            if (Math.Abs(BaseValue() - other.BaseValue()) > 1.0e-14)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Convert Quantity to target unit of measure
+        /// </summary>
+        /// <param name="target">Target unit of measure</param>
+        /// <returns>Quantity converted to target unit of measure</returns>
+        public Time As(Units target)
+        {
+            return new Time(Value * Factors[(int)Unit] / Factors[(int)target], target);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"{Value} {Symbols[(int)Unit]}";
+        }
+
+        /// <inheritdoc/>
+        public int CompareTo(Time other)
+        {
+            return BaseValue().CompareTo(other.BaseValue());
+        }
+
+        /// <inheritdoc/>
+        public string ToString(string? format)
+        {
+            return ToString(format, null);
+        }
+
+        /// <inheritdoc/>
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return $"{Value.ToString(format, formatProvider)} {Symbols[(int)Unit]}";
+        }
+
+        /// <inheritdoc/>
+        public Time Sqrt()
+        {
+            return new Time(Math.Sqrt(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Cbrt()
+        {
+            return new Time(Math.Cbrt(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Log()
+        {
+            return new Time(Math.Log(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Log2()
+        {
+            return new Time(Math.Log2(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Log10()
+        {
+            return new Time(Math.Log10(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Pow(double exp)
+        {
+            return new Time(Math.Pow(Value, exp), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Abs()
+        {
+            return new Time(Math.Abs(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Floor()
+        {
+            return new Time(Math.Floor(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Ceiling()
+        {
+            return new Time(Math.Ceiling(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Truncate()
+        {
+            return new Time(Math.Truncate(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Round()
+        {
+            return new Time(Math.Round(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Time Round(int digits)
+        {
+            return new Time(Math.Round(Value, digits), Unit);
+        }
+
+        /// <inheritdoc/>
+        public bool IsNegativeInfinity()
+        {
+            return Double.IsNegativeInfinity(Value);
+        }
+
+        /// <inheritdoc/>
+        public bool IsPositiveInfinity()
+        {
+            return Double.IsPositiveInfinity(Value);
+        }
+
+        /// <inheritdoc/>
+        public bool IsNaN()
+        {
+            return Double.IsNaN(Value);
+        }
+
+        /// <inheritdoc/>
+        public bool IsInfinity()
+        {
+            return Double.IsInfinity(Value);
+        }
+
+        /// <inheritdoc/>
+        public static Time Parse(string s, IFormatProvider? provider)
+        {
+            var tokens  = s.Split(' ', 2);
+
+            var value   = double.Parse(tokens[0]);
+            var unit    = (Units)Array.IndexOf(Symbols, tokens[1].Trim());
+
+            return new Time(value, unit);
+        }
+
+        /// <inheritdoc/>
+        public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Time result)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                result = default;
+                return false;
+            }
+
+            try
+            {
+                result = Parse(s, provider);
+                return true;
+            }
+            catch
+            {
+                result = default(Time);
+                return false;
+            }
+        }
+
+        #region OperatorOverloading
+
+        public static Time operator++(Time value)
+        {
+            return new Time(value.Value + 1, value.Unit);
+        }
+
+        public static Time operator--(Time value)
+        {
+            return new Time(value.Value - 1, value.Unit);
+        }
+
+        public static Time operator/(Time value, double scalar)
+        {
+            return new Time(value.Value / scalar, value.Unit);
+        }
+
+        public static Time operator*(Time value, double scalar)
+        {
+            return new Time(value.Value * scalar, value.Unit);
+        }
+
+        public static Time operator*(double scalar, Time value)
+        {
+            return new Time(value.Value * scalar, value.Unit);
+        }
+
+        public static Time operator-(Time a)
+        {
+            return new Time(-a.Value, a.Unit);
+        }
+
+        public static Time operator-(Time a, Time b)
+        {
+            if (a.Unit == b.Unit)
+            {
+                return new Time(a.Value - b.Value, a.Unit);
+            }
+
+            return new Time(a.BaseValue() - b.BaseValue(), BaseUnit);
+        }
+
+        public static Time operator+(Time a, Time b)
+        {
+            if (a.Unit == b.Unit)
+            {
+                return new Time(a.Value + b.Value, a.Unit);
+            }
+
+            return new Time(a.BaseValue() + b.BaseValue(), BaseUnit);
+        }
+
+        public static double operator/(Time a, Time b)
+        {
+            return a.BaseValue() / b.BaseValue();
+        }
+
+        public static bool operator==(Time a, Time b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator!=(Time a, Time b)
+        {
+            return !a.Equals(b);
+        }
+
+        #endregion
+    }
+}
---
+++ SI.Units.Net.UnitTests/TimeUnitTests.cs
@@ -0,0 +1,66 @@
+﻿namespace SI.Units.NET.UnitTests
+{
+    [TestClass]
+    public class TimeUnitTests
+    {
+        /// <summary> Double comparison tolerance </summary>
+        private const double EPS = 1.0e-12;
+        
+        [DataTestMethod]
+        [DataRow(100,  Time.Units.Second, Time.Units.Second,          100)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Decisecond,      1000)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Centisecond,     10000)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Millisecond,     100000)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Microsecond,     1e8)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Nanosecond,      1e11)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Picosecond,      1e14)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Femtosecond,     1e17)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Decasecond,      10)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Hectosecond,     1)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Kilosecond,      0.1)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Megasecond,      0.0001)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Gigasecond,      1e-7)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Terasecond,      1e-10)]
+        [DataRow(100,  Time.Units.Second, Time.Units.Petasecond,      1e-13)]
+        
+        [DataRow(60,    Time.Units.Second, Time.Units.Minute,          1.0)]
+        [DataRow(120,   Time.Units.Second, Time.Units.Minute,          2.0)]
+        [DataRow(180,   Time.Units.Second, Time.Units.Minute,          3.0)]
+        [DataRow(3600,  Time.Units.Second, Time.Units.Hour,           1.0)]
+        [DataRow(7200,  Time.Units.Second, Time.Units.Hour,           2.0)]
+        [DataRow(14400, Time.Units.Second, Time.Units.Hour,          4.0)]
+        [DataRow(86400, Time.Units.Second, Time.Units.Day,           1.0)]
+        
+
+        [DataRow(100,                   Time.Units.Second,         Time.Units.Second, 100)]
+        [DataRow(1000,                  Time.Units.Decisecond,     Time.Units.Second, 100)]
+        [DataRow(10000,                 Time.Units.Centisecond,    Time.Units.Second, 100)]
+        [DataRow(100000,                Time.Units.Millisecond,    Time.Units.Second, 100)]
+        [DataRow(1e8,                   Time.Units.Microsecond,    Time.Units.Second, 100)]
+        [DataRow(1e11,                  Time.Units.Nanosecond,     Time.Units.Second, 100)]
+        [DataRow(1e14,                  Time.Units.Picosecond,     Time.Units.Second, 100)]
+        [DataRow(1e17,                  Time.Units.Femtosecond,    Time.Units.Second, 100)]
+        [DataRow(10,                    Time.Units.Decasecond,     Time.Units.Second, 100)]
+        [DataRow(1,                     Time.Units.Hectosecond,    Time.Units.Second, 100)]
+        [DataRow(0.1,                   Time.Units.Kilosecond,     Time.Units.Second, 100)]
+        [DataRow(0.0001,                Time.Units.Megasecond,     Time.Units.Second, 100)]
+        [DataRow(1e-7,                  Time.Units.Gigasecond,     Time.Units.Second, 100)]
+        [DataRow(1e-10,                 Time.Units.Terasecond,     Time.Units.Second, 100)]
+        [DataRow(1e-13,                 Time.Units.Petasecond,     Time.Units.Second, 100)]
+        
+        [DataRow(1.0,   Time.Units.Minute, Time.Units.Second,          60)]
+        [DataRow(2.0,   Time.Units.Minute, Time.Units.Second,          120)]
+        [DataRow(3.0,   Time.Units.Minute, Time.Units.Second,          180)]
+        [DataRow(1.0,   Time.Units.Hour, Time.Units.Second,           3600)]
+        [DataRow(2.0,   Time.Units.Hour, Time.Units.Second,           7200)]
+        [DataRow(4.0,   Time.Units.Hour, Time.Units.Second,          14400)]
+        [DataRow(1.0,   Time.Units.Day, Time.Units.Second,           86400)]
+        public void AsTest(double value, Time.Units unit, Time.Units target, double expected)
+        {
+            var a = new Time(value, unit);
+            var b = a.As(target);
+
+            Assert.AreEqual(expected, b.Value, EPS);
+        }
+    }
+}
