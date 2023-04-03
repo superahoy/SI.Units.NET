e6e2f5ce18cc76a4590077901587a79766689894
John S Odom <john.s.odom@gmail.com>
2023-04-03 13:55:12 -0400

    Filling in Temperature units, adding unit tests to verify conversions.

--- SI.Units.NET/Temperature.cs
+++ SI.Units.NET/Temperature.cs
@@ -1,12 +1,394 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace SI.Units.NET
 {
-    internal class Temperature
+    /// <summary>
+    /// Represents Temperature Base Quantity type from Table 2. SI base units
+    /// </summary>
+    public readonly struct Temperature : IQuantity<Temperature>
     {
+        /// <summary> Base unit of measure </summary>
+        public const Units BaseUnit = Units.Kelvin;
+
+        /// <summary> Base symbol </summary>
+        public const string BaseSymbol = "°K";
+
+        /// <summary> Offset from 0°K to 0°C </summary>
+        public const double KELVIN2CELSIUS = -273.15;
+        
+        /// <summary> Offset from 0°C to 0°K </summary>
+        public const double CELSIUS2KELVIN = 273.15;
+
+        /// <summary> Offset from 0°R to 0°F </summary>
+        public const double RANKINE2FAHRENHEIT = -459.67;
+
+        /// <summary> Offset from 0°F to 0°R </summary>
+        public const double FAHRENHEIT2RANKINE = -459.67;
+
+        /// <summary> Supported units of measure for Temperature Quantity type </summary>
+        public enum Units
+        {
+            Kelvin,            
+            Celsius,
+            Fahrenheit,
+            Rankine
+        };
+
+        /// <summary>
+        /// Unit of measure symbols for each Unit. Order of
+        /// array must match Units order / int value
+        /// </summary>
+        private static readonly string[] Symbols =
+        {
+            BaseSymbol, // Kelvin
+            "°C",       // Celsius
+            "°F",       // Fahrenheit
+            "°R"        // Rankine
+        };
+
+        /// <summary>
+        /// Create new Temperature object
+        /// </summary>
+        /// <param name="value">Temperature value (amount)</param>
+        /// <param name="unit">Unit of measure value is in</param>
+        [JsonConstructor]
+        public Temperature(double value, Units unit)
+        {
+            Value   = value; 
+            Unit    = unit;
+        }
+
+        /// <inheritdoc/>
+        public double Value { get; init; }
+
+        /// <summary>
+        /// Unit of Measure value is in
+        /// </summary>
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Units Unit { get; init; }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return BaseValue().GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if ((obj is Temperature) == false) { return false; }
+            
+            return Equals((Temperature)obj);
+        }
+
+        /// <inheritdoc/>
+        public double BaseValue()
+        {
+            switch (Unit) 
+            {                 
+                case Units.Celsius:
+                    return Value + CELSIUS2KELVIN;
+                case Units.Fahrenheit:
+                    return (Value - 32.0) * 5.0/9.0 + CELSIUS2KELVIN;
+                case Units.Rankine:
+                    return (Value + RANKINE2FAHRENHEIT - 32.0) * 5.0/9.0 + CELSIUS2KELVIN;
+                case Units.Kelvin:
+                default:
+                    return Value;
+            }
+        }
+
+        /// <inheritdoc/>
+        public Temperature ToBase()
+        {
+            return As(BaseUnit);
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(Temperature other)
+        {
+            if (Math.Abs(BaseValue() - other.BaseValue()) > 1.0e-14)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Convert Quantity to target unit of measure
+        /// </summary>
+        /// <param name="target">Target unit of measure</param>
+        /// <returns>Quantity converted to target unit of measure</returns>
+        public Temperature As(Units target)
+        {
+            var kelvin = BaseValue();
+
+            switch (target) 
+            {
+                case Units.Celsius:
+                    return new Temperature(kelvin + KELVIN2CELSIUS, Units.Celsius);
+                case Units.Fahrenheit:
+                    return new Temperature((kelvin + KELVIN2CELSIUS) * 9.0/5.0 + 32, Units.Fahrenheit);
+                case Units.Rankine:
+                    return new Temperature(kelvin * 9.0 / 5.0, Units.Rankine);
+                case Units.Kelvin:
+                default:
+                    return new Temperature(kelvin, Units.Kelvin);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"{Value} {Symbols[(int)Unit]}";
+        }
+
+        /// <inheritdoc/>
+        public int CompareTo(Temperature other)
+        {
+            return BaseValue().CompareTo(other.BaseValue());
+        }
+
+        /// <inheritdoc/>
+        public string ToString(string? format)
+        {
+            return ToString(format, null);
+        }
+
+        /// <inheritdoc/>
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return $"{Value.ToString(format, formatProvider)} {Symbols[(int)Unit]}";
+        }
+
+        /// <inheritdoc/>
+        public Temperature Sqrt()
+        {
+            return new Temperature(Math.Sqrt(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Cbrt()
+        {
+            return new Temperature(Math.Cbrt(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Log()
+        {
+            return new Temperature(Math.Log(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Log2()
+        {
+            return new Temperature(Math.Log2(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Log10()
+        {
+            return new Temperature(Math.Log10(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Pow(double exp)
+        {
+            return new Temperature(Math.Pow(Value, exp), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Abs()
+        {
+            return new Temperature(Math.Abs(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Floor()
+        {
+            return new Temperature(Math.Floor(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Ceiling()
+        {
+            return new Temperature(Math.Ceiling(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Truncate()
+        {
+            return new Temperature(Math.Truncate(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Round()
+        {
+            return new Temperature(Math.Round(Value), Unit);
+        }
+
+        /// <inheritdoc/>
+        public Temperature Round(int digits)
+        {
+            return new Temperature(Math.Round(Value, digits), Unit);
+        }
+
+        /// <inheritdoc/>
+        public bool IsNegativeInfinity()
+        {
+            return Double.IsNegativeInfinity(Value);
+        }
+
+        /// <inheritdoc/>
+        public bool IsPositiveInfinity()
+        {
+            return Double.IsPositiveInfinity(Value);
+        }
+
+        /// <inheritdoc/>
+        public bool IsNaN()
+        {
+            return Double.IsNaN(Value);
+        }
+
+        /// <inheritdoc/>
+        public bool IsInfinity()
+        {
+            return Double.IsInfinity(Value);
+        }
+
+        /// <inheritdoc/>
+        public int Sign()
+        {
+            return Math.Sign(Value);
+        }
+
+        /// <inheritdoc/>
+        public static Temperature Parse(string s, IFormatProvider? provider)
+        {
+            var tokens  = s.Split(' ', 2);
+
+            var value   = double.Parse(tokens[0]);
+            var unit    = (Units)Array.IndexOf(Symbols, tokens[1].Trim());
+
+            return new Temperature(value, unit);
+        }
+
+        /// <inheritdoc/>
+        public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Temperature result)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                result = default;
+                return false;
+            }
+
+            try
+            {
+                result = Parse(s, provider);
+                return true;
+            }
+            catch
+            {
+                result = default(Temperature);
+                return false;
+            }
+        }
+
+        #region OperatorOverloading
+
+        public static Temperature operator++(Temperature value)
+        {
+            return new Temperature(value.Value + 1, value.Unit);
+        }
+
+        public static Temperature operator--(Temperature value)
+        {
+            return new Temperature(value.Value - 1, value.Unit);
+        }
+
+        public static Temperature operator/(Temperature value, double scalar)
+        {
+            return new Temperature(value.Value / scalar, value.Unit);
+        }
+
+        public static Temperature operator*(Temperature value, double scalar)
+        {
+            return new Temperature(value.Value * scalar, value.Unit);
+        }
+
+        public static Temperature operator*(double scalar, Temperature value)
+        {
+            return new Temperature(value.Value * scalar, value.Unit);
+        }
+
+        public static Temperature operator-(Temperature a)
+        {
+            return new Temperature(-a.Value, a.Unit);
+        }
+
+        public static Temperature operator-(Temperature a, Temperature b)
+        {
+            if (a.Unit == b.Unit)
+            {
+                return new Temperature(a.Value - b.Value, a.Unit);
+            }
+
+            return new Temperature(a.BaseValue() - b.BaseValue(), BaseUnit);
+        }
+
+        public static Temperature operator+(Temperature a, Temperature b)
+        {
+            if (a.Unit == b.Unit)
+            {
+                return new Temperature(a.Value + b.Value, a.Unit);
+            }
+
+            return new Temperature(a.BaseValue() + b.BaseValue(), BaseUnit);
+        }
+
+        public static double operator/(Temperature a, Temperature b)
+        {
+            return a.BaseValue() / b.BaseValue();
+        }
+
+        public static bool operator==(Temperature a, Temperature b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator!=(Temperature a, Temperature b)
+        {
+            return !a.Equals(b);
+        }
+
+        public static bool operator>(Temperature a, Temperature b)
+        {
+            return a.BaseValue() > b.BaseValue();
+        }
+
+        public static bool operator<(Temperature a, Temperature b)
+        {
+            return a.BaseValue() < b.BaseValue();
+        }
+
+        public static bool operator>=(Temperature a, Temperature b)
+        {
+            return a.BaseValue() >= b.BaseValue();
+        }
+
+        public static bool operator<=(Temperature a, Temperature b)
+        {
+            return a.BaseValue() <= b.BaseValue();
+        }
+
+        public static Temperature operator%(Temperature a, double b)
+        {
+            return new Temperature(a.Value % b, a.Unit);
+        }
+
+        #endregion
     }
 }
---
+++ SI.Units.Net.UnitTests/TemperatureUnitTests.cs
@@ -0,0 +1,101 @@
+﻿namespace SI.Units.NET.UnitTests
+{
+    [TestClass]
+    public class TemperatureUnitTests
+    {
+        public const double EPS = 0.001;
+
+        [DataTestMethod]
+        [DataRow(  0, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 32.0)]
+        [DataRow( 10, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 50.0)]
+        [DataRow( 20, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 68.0)]
+        [DataRow( 30, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 86.0)]
+        [DataRow( 40, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 104.0)]
+        [DataRow( 50, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 122.0)]
+        [DataRow( 60, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 140.0)]
+        [DataRow( 70, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 158.0)]
+        [DataRow( 80, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 176.0)]
+        [DataRow( 90, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 194.0)]
+        [DataRow(100, Temperature.Units.Celsius, Temperature.Units.Fahrenheit, 212.0)]
+
+        [DataRow(  0, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -459.67)]
+        [DataRow( 10, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -441.67)]
+        [DataRow( 20, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -423.67)]
+        [DataRow( 30, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -405.67)]
+        [DataRow( 40, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -387.67)]
+        [DataRow( 50, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -369.67)]
+        [DataRow( 60, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -351.67)]
+        [DataRow( 70, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -333.67)]
+        [DataRow( 80, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -315.67)]
+        [DataRow( 90, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -297.67)]
+        [DataRow(100, Temperature.Units.Kelvin, Temperature.Units.Fahrenheit, -279.67)]
+
+        [DataRow(  0, Temperature.Units.Kelvin, Temperature.Units.Celsius, -273.15)]
+        [DataRow( 10, Temperature.Units.Kelvin, Temperature.Units.Celsius, -263.15)]
+        [DataRow( 20, Temperature.Units.Kelvin, Temperature.Units.Celsius, -253.15)]
+        [DataRow( 30, Temperature.Units.Kelvin, Temperature.Units.Celsius, -243.15)]
+        [DataRow( 40, Temperature.Units.Kelvin, Temperature.Units.Celsius, -233.15)]
+        [DataRow( 50, Temperature.Units.Kelvin, Temperature.Units.Celsius, -223.15)]
+        [DataRow( 60, Temperature.Units.Kelvin, Temperature.Units.Celsius, -213.15)]
+        [DataRow( 70, Temperature.Units.Kelvin, Temperature.Units.Celsius, -203.15)]
+        [DataRow( 80, Temperature.Units.Kelvin, Temperature.Units.Celsius, -193.15)]
+        [DataRow( 90, Temperature.Units.Kelvin, Temperature.Units.Celsius, -183.15)]
+        [DataRow(100, Temperature.Units.Kelvin, Temperature.Units.Celsius, -173.15)]
+        [DataRow(200, Temperature.Units.Kelvin, Temperature.Units.Celsius, -073.15)]
+        [DataRow(300, Temperature.Units.Kelvin, Temperature.Units.Celsius,  026.85)]
+        [DataRow(400, Temperature.Units.Kelvin, Temperature.Units.Celsius,  126.85)]
+        [DataRow(500, Temperature.Units.Kelvin, Temperature.Units.Celsius,  226.85)]
+        [DataRow(600, Temperature.Units.Kelvin, Temperature.Units.Celsius,  326.85)]
+
+        [DataRow(  0, Temperature.Units.Kelvin, Temperature.Units.Rankine,    0)]
+        [DataRow( 10, Temperature.Units.Kelvin, Temperature.Units.Rankine,   18)]
+        [DataRow( 20, Temperature.Units.Kelvin, Temperature.Units.Rankine,   36)]
+        [DataRow( 30, Temperature.Units.Kelvin, Temperature.Units.Rankine,   54)]
+        [DataRow( 40, Temperature.Units.Kelvin, Temperature.Units.Rankine,   72)]
+        [DataRow( 50, Temperature.Units.Kelvin, Temperature.Units.Rankine,   90)]
+        [DataRow( 60, Temperature.Units.Kelvin, Temperature.Units.Rankine,  108)]
+        [DataRow( 70, Temperature.Units.Kelvin, Temperature.Units.Rankine,  126)]
+        [DataRow( 80, Temperature.Units.Kelvin, Temperature.Units.Rankine,  144)]
+        [DataRow( 90, Temperature.Units.Kelvin, Temperature.Units.Rankine,  162)]
+        [DataRow(100, Temperature.Units.Kelvin, Temperature.Units.Rankine,  180)]
+        [DataRow(200, Temperature.Units.Kelvin, Temperature.Units.Rankine,  360)]
+        [DataRow(300, Temperature.Units.Kelvin, Temperature.Units.Rankine,  540)]
+        [DataRow(400, Temperature.Units.Kelvin, Temperature.Units.Rankine,  720)]
+        [DataRow(500, Temperature.Units.Kelvin, Temperature.Units.Rankine,  900)]
+        [DataRow(600, Temperature.Units.Kelvin, Temperature.Units.Rankine, 1080)]
+
+        [DataRow(  0, Temperature.Units.Rankine, Temperature.Units.Celsius, -273.150)]
+        [DataRow( 10, Temperature.Units.Rankine, Temperature.Units.Celsius, -267.594)]
+        [DataRow( 20, Temperature.Units.Rankine, Temperature.Units.Celsius, -262.039)]
+        [DataRow( 30, Temperature.Units.Rankine, Temperature.Units.Celsius, -256.483)]
+        [DataRow( 40, Temperature.Units.Rankine, Temperature.Units.Celsius, -250.928)]
+        [DataRow( 50, Temperature.Units.Rankine, Temperature.Units.Celsius, -245.372)]
+        [DataRow( 60, Temperature.Units.Rankine, Temperature.Units.Celsius, -239.817)]
+        [DataRow( 70, Temperature.Units.Rankine, Temperature.Units.Celsius, -234.261)]
+        [DataRow( 80, Temperature.Units.Rankine, Temperature.Units.Celsius, -228.706)]
+        [DataRow( 90, Temperature.Units.Rankine, Temperature.Units.Celsius, -223.150)]
+        [DataRow(100, Temperature.Units.Rankine, Temperature.Units.Celsius, -217.594)]
+        [DataRow(200, Temperature.Units.Rankine, Temperature.Units.Celsius, -162.039)]
+        [DataRow(300, Temperature.Units.Rankine, Temperature.Units.Celsius, -106.483)]
+        [DataRow(400, Temperature.Units.Rankine, Temperature.Units.Celsius, -50.9278)]
+        [DataRow(500, Temperature.Units.Rankine, Temperature.Units.Celsius,  4.62778)]
+        [DataRow(600, Temperature.Units.Rankine, Temperature.Units.Celsius,  60.1833)]
+
+        public void AsTest(double value, Temperature.Units unit, Temperature.Units target, double expected)
+        {
+            // target conversion
+
+            var a = new Temperature(value, unit);
+            var b = a.As(target);
+
+            Assert.AreEqual(expected, b.Value, EPS);
+
+            // reverse conversion
+
+            var c = new Temperature(expected, target);
+            var d = c.As(unit);
+
+            Assert.AreEqual(value, d.Value, EPS);
+        }
+    }
+}
